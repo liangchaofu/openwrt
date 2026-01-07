4ff13fd9cb77120610fd74f459746c6a267be981
liangchaofu <70999087+liangchaofu@users.noreply.github.com>
2026-01-07 19:40:05 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,4 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 
 git clone https://github.com/liangchaofu/package.git package/lcf
-tar -zxvf lcf.tar.gz -C /package
+tar -zxvf package/lcf/lcf.tar.gz -C /package
