c6628345877758f504790adf6d34cf03c0e5097d
liangchaofu <70999087+liangchaofu@users.noreply.github.com>
2026-01-09 14:48:02 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,4 +19,4 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-cd feeds/luci/applications/luci-app-dockerman && rm -rf ./ && cd.. && cd luci-lib-docker && rm -rf ./ && cd.. && cd.. && cd.. && cd packages/net/shadowsocks-libev && rm -rf ./ && cd.. && cd kcptun && rm -rf ./
+cd feeds/luci/applications/luci-app-dockerman && rm -rf ./* && cd.. && cd luci-lib-docker && rm -rf ./* && cd.. && cd.. && cd.. && cd packages/net/shadowsocks-libev && rm -rf ./* && cd.. && cd kcptun && rm -rf ./*
