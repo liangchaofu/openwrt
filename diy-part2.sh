3272f27f03d46e0b756f646f1453da3c104a2053
liangchaofu <70999087+liangchaofu@users.noreply.github.com>
2026-01-11 23:40:49 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -19,8 +19,7 @@
 # Modify hostname
 #sed -i 's/OpenWrt/P3TERX-Router/g' package/base-files/files/bin/config_generate
 
-cd feeds/luci/applications/luci-app-dockerman && rm -rf ./* && cd ../.. && cd collections/luci-lib-docker && rm -rf ./* && cd ../../.. && cd packages/net/shadowsocks-libev && rm -rf ./* && cd .. && cd kcptun && rm -rf ./*
-mv -vf kernel.config config/.config
-#mv -vf .vermagic .
+cd feeds/luci/applications/luci-app-dockerman && rm -rf ./* && cd ../.. && cd collections/luci-lib-docker && rm -rf ./* && cd ../../.. && cd packages/net/shadowsocks-libev && rm -rf ./* && cd .. && cd kcptun && rm -rf ./* && cd ../../../.. && mv -vf ../kernel.config config/.config
+mv -vf .vermagic .
 mv -vf kernel-defaults.mk.txt include/kernel-defaults.mk
 mv -vf Makefile.txt package/kernel/linux/Makefile
